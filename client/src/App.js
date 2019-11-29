535d949127277edec0a5ff036e6751be1523fdc5
Anand Chembarpu <anandchembarpu@gmail.com>
2019-11-29 09:51:03 +0530

    Setup basic website

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 import Home from "./pages/Home";
-
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div>
-        <Home />
-      </div>
-    </ApolloProvider>
+    <Router>
+      <Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
